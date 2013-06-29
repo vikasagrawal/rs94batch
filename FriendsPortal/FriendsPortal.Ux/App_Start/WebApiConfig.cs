fdcbf86e3917851e4f6a953640ae6db9a8efd382
VIKAS AGRAWAL <vikasaagrawal@yahoo.com>
2013-06-29 12:03:31 +0530

    initial commit

---
+++ FriendsPortal/FriendsPortal.Ux/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace FriendsPortal.Ux
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ FriendsPortal/FriendsPortal.Ux/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FriendsPortal.Ux.MvcApplication" Language="C#" %>
