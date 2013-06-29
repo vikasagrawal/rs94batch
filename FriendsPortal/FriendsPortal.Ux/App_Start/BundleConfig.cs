b4f51a30ed4f8995ed8c5acba7bf2ac124f1b6fa
VIKAS AGRAWAL <vikasaagrawal@yahoo.com>
2013-06-29 14:28:16 +0530

    commit

--- FriendsPortal/FriendsPortal.Ux/App_Start/BundleConfig.cs
+++ FriendsPortal/FriendsPortal.Ux/App_Start/BundleConfig.cs
@@ -18,13 +18,19 @@ namespace FriendsPortal.Ux
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                        "~/Scripts/knockout-{version}.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
+            //bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Scripts/twitter-bootstrap/docs/assets/css/bootstrap.css",
+                "~/Scripts/twitter-bootstrap/docs/assets/css/bootstrap-responsive.css"));
+            
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
                         "~/Content/themes/base/jquery.ui.resizable.css",
